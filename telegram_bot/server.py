1e4541a994c270c6a2c8004ac1e1be725a498c06
Coolworld <coolworld2049@gmail.com>
2022-03-28 07:48:53 +0300

    heroku depl

--- telegram_bot/server.py
+++ telegram_bot/server.py
@@ -24,35 +24,7 @@ dp.middleware.setup(LoggingMiddleware())
 # ---vk_api
 downloadVk = DownloadVk()
 
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
-# webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-# webserver settings
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
-
-
-async def on_startup(dispather):
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
-
-
-async def on_shutdown(dispather):
-    await bot.delete_webhook()
-
-
-start_webhook(
-    dispatcher=dp,
-    webhook_path=WEBHOOK_PATH,
-    on_startup=on_startup,
-    on_shutdown=on_shutdown,
-    skip_updates=False,
-    host=WEBAPP_HOST,
-    port=WEBAPP_PORT,
-)
 
 
 @dp.message_handler(commands=['start'])
